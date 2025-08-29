5930fe816a09f45a5895aabc82b210d2f8b2ab53
ChethanJS07 <jschethan75@gmail.com>
2025-08-29 11:01:05 +0530

    initialization

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  res.send("BFHL API is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
