77ce6f384067c231652d3bd818c2a5958a7d5cbc
ChethanJS07 <jschethan75@gmail.com>
2025-08-29 11:21:20 +0530

    used es module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+import express, { json } from "express";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 
 const FULL_NAME = "ChethanJS";
 const DOB = "07052005";
