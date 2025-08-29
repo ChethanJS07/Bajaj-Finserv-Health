292b63c58f139d173ef2a22034c9ed5e4186202f
ChethanJS07 <jschethan75@gmail.com>
2025-08-29 11:02:09 +0530

    added / path

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("BFHL API is running!");
+});
 
 app.get("/", (req, res) => {
   res.send("BFHL API is running!");
